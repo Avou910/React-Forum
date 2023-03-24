d4a8d4f0ea4c126cb51d5e26a833fb13c82928ce
Rami Valkama <rami.valkama@tuni.fi>
2023-03-24 17:58:50 +0200

    ä¨additions to firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,20 +1,18 @@
 /* global firebase */
 
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 
 // Your web app's Firebase configuration
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyBKZGs-AYq1fdKdR88UwKw_vbulBsgzQDk",
 
   authDomain: "pokemon-forum-47afd.firebaseapp.com",
@@ -27,20 +25,19 @@ const firebaseConfig = {
 
   appId: "1:450310409059:web:51ac404628cd3560a0a156",
 
-  measurementId: "G-08ZWJWQGC4"
-
+  measurementId: "G-08ZWJWQGC4",
 };
-
 
 // Initialize Firebase
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
 
 //const analytics = getAnalytics(app);
 
 export const auth = firebase.auth();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
